14411ea10ff527293eec8d4915e7fd38b1d7ccfe
muirawachanga <wachangasteve@gmail.com>
2017-08-01 15:51:12 +0300

    commiting after the first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+import json
+data={"name":"stephen",
+"school":"University of Eldoret",
+"Employement":"Bituls Company Ltd",
+"Project":"Understanding Gitflow"
+}
+for nr in data:
+    dat=nr["name"]
+    print(data)
